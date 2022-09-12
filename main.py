303cf4176da69a4bd962730479318ad99492360d
Bryan <bhowiebkr@gmail.com>
2022-09-12 00:35:34 -0400

    material widget and update func

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import logging
 
 from PySide6 import QtWidgets, QtCore, QtGui
 
-from lib.palette import palette
+from components.palette import palette
+from components.widgets import MaterialCombo
 
 
 class GUI(QtWidgets.QMainWindow):
@@ -27,15 +28,42 @@ class GUI(QtWidgets.QMainWindow):
         main_widget = QtWidgets.QWidget()
         main_layout = QtWidgets.QHBoxLayout()
 
+        self.setCentralWidget(main_widget)
+        main_widget.setLayout(main_layout)
+
+        form = QtWidgets.QFormLayout()
+
+
+        # widgets
+        self.materialCombo = MaterialCombo()
+
+
+        # Add Widgets
+        form.addRow('Material', self.materialCombo)
+        main_layout.addLayout(form)
+        
+
+
+        # Logic
+        self.materialCombo.currentIndexChanged.connect(self.update)
+
+        self.update()
+
     def closeEvent(self, event):
         self.settings = QtCore.QSettings("speeds-and-feeds-calc", "SpeedsAndFeedsCalculator")
         self.settings.setValue("geometry", self.saveGeometry())
         QtWidgets.QWidget.closeEvent(self, event)
 
+    def update(self):
+        print('update')
+        print('Material', self.materialCombo.material)
+        print('HB Min', self.materialCombo.HBMin)
+        print('HB Max', self.materialCombo.HBMax)
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    app.setPalette(palette)
+    #app.setPalette(palette)
     gui = GUI()
     gui.show()
     app.exec()
