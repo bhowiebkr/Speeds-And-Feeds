7d845fb689c7bbbcc47705dd4fb89ea9a410c1ef
Bryan <bhowiebkr@gmail.com>
2022-09-12 02:38:33 -0400

    results section

--- main.py
+++ main.py
@@ -12,21 +12,21 @@ from components.widgets import MaterialCombo, IntInput, DoubleInput
 class ToolBox(QtWidgets.QGroupBox):
     def __init__(self, parent=None):
         super(ToolBox, self).__init__(parent)
-        self.setTitle('Tool')
+        self.setTitle('Tool Info')
         form = QtWidgets.QFormLayout()
         self.setLayout(form)
 
         # Widgets
-        self.toolDiameter = DoubleInput(6.0)
-        self.fluteNum = IntInput(1)
-        self.fluteLen = DoubleInput(15.0)
-        self.leadAngle = DoubleInput(90.0)
+        self.toolDiameter = QtWidgets.QDoubleSpinBox()
+        self.fluteNum = QtWidgets.QSpinBox()
+        self.fluteLen = QtWidgets.QDoubleSpinBox()
+        self.leadAngle = QtWidgets.QDoubleSpinBox()
 
         # Add Layout
-        form.addRow('Diameter (MM):', self.toolDiameter)
-        form.addRow('Number of flutes:', self.fluteNum)
-        form.addRow('Flute Length:', self.fluteLen)
-        form.addRow('Lead Angle:', self.leadAngle)
+        form.addRow('Diameter (MM)', self.toolDiameter)
+        form.addRow('Number of flutes (#)', self.fluteNum)
+        form.addRow('Flute Length (MM)', self.fluteLen)
+        form.addRow('Lead Angle (°)', self.leadAngle)
 
 
 
@@ -38,33 +38,95 @@ class CuttingBox(QtWidgets.QGroupBox):
         self.setLayout(form)
 
         # Widgets
-        self.DOC = DoubleInput(1.5)
-        self.WOC = DoubleInput(2)
+        self.DOC = QtWidgets.QDoubleSpinBox()
+        self.WOC = QtWidgets.QDoubleSpinBox()
         self.useWidthPercentage = QtWidgets.QCheckBox()
-        self.widthPercentage = DoubleInput()
+        self.widthPercentage = QtWidgets.QDoubleSpinBox()
         self.slotting = QtWidgets.QCheckBox()
 
-        form.addRow('Depth Of Cut', self.DOC)
-        form.addRow('Width Of Cut', self.WOC)
-        form.addRow('Use Width Percentage', self.useWidthPercentage)
-        form.addRow('Width Percentage', self.widthPercentage)
+        form.addRow('Depth Of Cut (MM)', self.DOC)
+        form.addRow('Width Of Cut (MM)', self.WOC)
+        form.addRow('Width As Percentage', self.useWidthPercentage)
+        form.addRow('Width Percentage (%)', self.widthPercentage)
         form.addRow('Slotting', self.slotting)
+
+        self.useWidthPercentage.stateChanged.connect(self.updateGUI)
+        self.slotting.stateChanged.connect(self.updateGUI)
+
+        self.updateGUI()
+
+    def updateGUI(self):
+
+        if self.slotting.isChecked():
+            self.WOC.setDisabled(True)
+            self.widthPercentage.setDisabled(True)
+            self.useWidthPercentage.setDisabled(True)
+            return
+        else:
+            self.useWidthPercentage.setEnabled(True)
+
+
+        if self.useWidthPercentage.isChecked():
+            self.WOC.setDisabled(True)
+            self.widthPercentage.setEnabled(True)
+        else:
+            self.WOC.setEnabled(True)
+            self.widthPercentage.setDisabled(True)
+
+
+
 
 class MachineBox(QtWidgets.QGroupBox):
     def __init__(self, parent=None):
         super(MachineBox, self).__init__(parent)
-        self.setTitle('Machine')
+        self.setTitle('Machine Specs')
         form = QtWidgets.QFormLayout()
         self.setLayout(form)
 
         # Widgets
-        self.minRPM = IntInput(6000)
-        self.maxRPM = IntInput(24000)
+        self.minRPM = QtWidgets.QSpinBox()
+        self.minRPM.setMaximum(100000)
+        self.minRPM.setValue(6000)
+        self.maxRPM = QtWidgets.QSpinBox()
+        self.maxRPM.setMaximum(100000)
+        self.maxRPM.setValue(24000)
 
-        form.addRow('Min RPM', self.minRPM)
-        form.addRow('Max RPM', self.maxRPM)
+        form.addRow('Min (RPM)', self.minRPM)
+        form.addRow('Max (RPM)', self.maxRPM)
 
 
+class ResultsBox(QtWidgets.QGroupBox):
+    def __init__(self, parent=None):
+        super(ResultsBox, self).__init__(parent)
+        self.setTitle('Results')
+        mainLayout = QtWidgets.QHBoxLayout()
+        self.setLayout(mainLayout)
+
+        formLeft = QtWidgets.QFormLayout()
+        formRight = QtWidgets.QFormLayout()
+
+        for form in [formLeft, formRight]:
+            form.setLabelAlignment(QtCore.Qt.AlignRight)
+
+        mainLayout.addStretch()
+        mainLayout.addLayout(formLeft)
+        mainLayout.addStretch()
+        mainLayout.addLayout(formRight)
+        mainLayout.addStretch()
+
+        # Widgets
+        self.rpm = QtWidgets.QLabel('<b>18,765</b>')
+        self.feed = QtWidgets.QLabel('<b>2000 mm/min</b>')
+        self.cutSpeed = QtWidgets.QLabel('<b>10 m/min</b>')
+        self.chipLoad = QtWidgets.QLabel('<b>0.065</b>')
+        self.mmr = QtWidgets.QLabel('<b>62 cm³/min</b>') 
+
+        formLeft.addRow('RPM (n):', self.rpm)
+        formLeft.addRow('Surface Speed (Vc):', self.cutSpeed)
+        formLeft.addRow('Material Removal Rate (MMR):', self.mmr)
+        formRight.addRow('Feed (f):', self.feed)
+        formRight.addRow('Chip Load (fz):', self.chipLoad)
+ 
 
 
 class GUI(QtWidgets.QMainWindow):
@@ -72,7 +134,7 @@ class GUI(QtWidgets.QMainWindow):
         super(GUI, self).__init__(parent)
         self.settings = None
 
-        self.setWindowTitle("Speeds and Feeds Calculator")
+        self.setWindowTitle("Speeds and Feeds Calculator - https://github.com/bhowiebkr/Speeds-And-Feeds")
         settings = QtCore.QSettings("speeds-and-feeds-calc", "SpeedsAndFeedsCalculator")
 
         try:
@@ -96,6 +158,8 @@ class GUI(QtWidgets.QMainWindow):
         self.cutting_box = CuttingBox()
         self.machine_box = MachineBox()
 
+        self.results_box = ResultsBox()
+
 
 
         # widgets
@@ -109,6 +173,7 @@ class GUI(QtWidgets.QMainWindow):
         sections_layout.addWidget(self.tool_box)
         sections_layout.addWidget(self.cutting_box)
         sections_layout.addWidget(self.machine_box)
+        main_layout.addWidget(self.results_box)
         
 
 
