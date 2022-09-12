c4ab91e568bd6c58840c12cac1f9ab1d91f8008d
Bryan <bhowiebkr@gmail.com>
2022-09-12 01:29:42 -0400

    working out the layout

--- components/widgets.py
+++ components/widgets.py
@@ -34,6 +34,18 @@ class MaterialCombo(QtWidgets.QComboBox):
 
 
 
+class IntInput(QtWidgets.QLineEdit):
+    def __init__(self, val=None):
+        super(IntInput, self).__init__(None)
+        self.setValidator(QtGui.QIntValidator())
 
+        if val:
+            self.setText(str(val))
         
-
+class DoubleInput(QtWidgets.QLineEdit):
+    def __init__(self, val=None):
+        super(DoubleInput, self).__init__(None)
+        self.setValidator(QtGui.QDoubleValidator())
+        
+        if val:
+            self.setText(str(val))
--- main.py
+++ main.py
@@ -5,7 +5,66 @@ import logging
 from PySide6 import QtWidgets, QtCore, QtGui
 
 from components.palette import palette
-from components.widgets import MaterialCombo
+from components.widgets import MaterialCombo, IntInput, DoubleInput
+
+
+
+class ToolBox(QtWidgets.QGroupBox):
+    def __init__(self, parent=None):
+        super(ToolBox, self).__init__(parent)
+        self.setTitle('Tool')
+        form = QtWidgets.QFormLayout()
+        self.setLayout(form)
+
+        # Widgets
+        self.toolDiameter = DoubleInput(6.0)
+        self.fluteNum = IntInput(1)
+        self.fluteLen = DoubleInput(15.0)
+        self.leadAngle = DoubleInput(90.0)
+
+        # Add Layout
+        form.addRow('Diameter (MM):', self.toolDiameter)
+        form.addRow('Number of flutes:', self.fluteNum)
+        form.addRow('Flute Length:', self.fluteLen)
+        form.addRow('Lead Angle:', self.leadAngle)
+
+
+
+class CuttingBox(QtWidgets.QGroupBox):
+    def __init__(self, parent=None):
+        super(CuttingBox, self).__init__(parent)
+        self.setTitle('Cutting Operation')
+        form = QtWidgets.QFormLayout()
+        self.setLayout(form)
+
+        # Widgets
+        self.DOC = DoubleInput(1.5)
+        self.WOC = DoubleInput(2)
+        self.useWidthPercentage = QtWidgets.QCheckBox()
+        self.widthPercentage = DoubleInput()
+        self.slotting = QtWidgets.QCheckBox()
+
+        form.addRow('Depth Of Cut', self.DOC)
+        form.addRow('Width Of Cut', self.WOC)
+        form.addRow('Use Width Percentage', self.useWidthPercentage)
+        form.addRow('Width Percentage', self.widthPercentage)
+        form.addRow('Slotting', self.slotting)
+
+class MachineBox(QtWidgets.QGroupBox):
+    def __init__(self, parent=None):
+        super(MachineBox, self).__init__(parent)
+        self.setTitle('Machine')
+        form = QtWidgets.QFormLayout()
+        self.setLayout(form)
+
+        # Widgets
+        self.minRPM = IntInput(6000)
+        self.maxRPM = IntInput(24000)
+
+        form.addRow('Min RPM', self.minRPM)
+        form.addRow('Max RPM', self.maxRPM)
+
+
 
 
 class GUI(QtWidgets.QMainWindow):
@@ -26,12 +85,17 @@ class GUI(QtWidgets.QMainWindow):
 
         # Layouts
         main_widget = QtWidgets.QWidget()
-        main_layout = QtWidgets.QHBoxLayout()
+        main_layout = QtWidgets.QVBoxLayout()
+        sections_layout = QtWidgets.QHBoxLayout()
 
         self.setCentralWidget(main_widget)
         main_widget.setLayout(main_layout)
-
         form = QtWidgets.QFormLayout()
+
+        self.tool_box = ToolBox()
+        self.cutting_box = CuttingBox()
+        self.machine_box = MachineBox()
+
 
 
         # widgets
@@ -41,6 +105,10 @@ class GUI(QtWidgets.QMainWindow):
         # Add Widgets
         form.addRow('Material', self.materialCombo)
         main_layout.addLayout(form)
+        main_layout.addLayout(sections_layout)
+        sections_layout.addWidget(self.tool_box)
+        sections_layout.addWidget(self.cutting_box)
+        sections_layout.addWidget(self.machine_box)
         
 
 
